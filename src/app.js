b60f7b3e439a1485083c97dd16725e5eea0cde0c
hadiPU <hadiprio575@gmail.com>
2026-05-31 01:12:25 +0700

    update url cors

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@ const app = express();
 app.use(cors({
   origin: [
     "https://sortirin.vercel.app",
+    "http://sortirin.oxidilily.online",
     "http://localhost:5173",
-    "http://localhost:3000",
   ],
   credentials: true,
 }));
